1920026d10c6abbb046a398b9f40ae20c8e3ff6b
prathan1234 <prathan.psong@gmail.com>
2017-12-04 18:53:17 +0700

    Update lastest version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { HellloworldComponent } from './components/hellloworld/hellloworld.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
-import { FormsModule } from '@angular/forms'
-import { GetphotoService } from './services/getphoto.service'
-import { HttpModule } from '@angular/http'
-import { RouterModule, Routes } from '@angular/router';
-import { HellloworldComponent } from './components/hellloworld/hellloworld.component'
+import { LoginService } from './services/login.service'
+import { GetphotoService } from './services/getphoto.service' 
+import { UserManagementService } from './services/user-management.service'
+
+import { AuthguardGuard } from './authguard.guard';
 
 const appRoutes:Routes = [
-  {path:"", component:UserComponent},
-  {path:"about", component:AboutusComponent},
-  {path:"helloworld", component:HellloworldComponent}
+  {path: '',component: LoginComponent},
+  {path: 'home',canActivate: [AuthguardGuard],component: HomeComponent},
+  {path: 'user',canActivate: [AuthguardGuard],component: UserComponent},
+  {path: 'aboutus',canActivate: [AuthguardGuard],component: AboutusComponent},
+  {path: 'user-management',component: UserManagementComponent},
+  {path: '**',canActivate: [AuthguardGuard],component: LoginComponent}
 ]
 
 @NgModule({
@@ -22,7 +34,12 @@ const appRoutes:Routes = [
     AppComponent,
     UserComponent,
     AboutusComponent,
-    HellloworldComponent
+    HellloworldComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +47,12 @@ const appRoutes:Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [ GetphotoService ],
+  providers: [ 
+    GetphotoService, 
+    LoginService,
+    AuthguardGuard,
+    UserManagementService  
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
